eea4ba7220284e88d3fcc92bf14acad6b69bcb9c
Ubuntu <ubuntu@ip-172-31-15-25.us-west-2.compute.internal>
2026-01-28 09:04:15 +0000

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/src
+# Build directory: /home/ubuntu/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("location")
+subdirs("my_robot")
+subdirs("teleop_twist_keyboard")
